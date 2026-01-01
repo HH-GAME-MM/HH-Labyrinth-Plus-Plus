615a73cd57406ea4909c1d98756f80b38f1ae083
xnh0x <xnh0x@pm.me>
2026-01-02 00:05:04 +0100

    version 0.12.1

--- HH-Labyrinth-Plus-Plus.user.js
+++ HH-Labyrinth-Plus-Plus.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         HH Labyrinth++
-// @version      0.12.0
+// @version      0.12.1
 // @description  Upgrade Labyrinth, WBT and PD with various features
 // @author       -MM-, xnh0x
 // @match        https://*.hentaiheroes.com/labyrinth.html*
